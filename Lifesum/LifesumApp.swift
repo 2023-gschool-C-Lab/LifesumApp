f7878be4589d34d8a2f2ae2d102100d0b3fcb49a
jiwoon <jwgil06@naver.com>
2023-06-26 10:18:07 +0900

    Initial Commit

---
+++ Lifesum/LifesumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifesumApp.swift
+//  Lifesum
+//
+//  Created by jeewony on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct LifesumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
